33b18f4951b5f886bb6a2d26fd00bbc789de1b92
dettlaffb <209295@edu.p.lodz.pl>
2019-04-29 17:11:28 +0200

    Drugie zadanie - wyswietlenie GitHub ID - bdettlaff

--- src/com/epaxera/Main.java
+++ src/com/epaxera/Main.java
@@ -3,6 +3,6 @@ package com.epaxera;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("EPAxERA Operations Meshpy - TeamLeader");
+        System.out.println("EPAxERA Developer bdettlaff");
     }
 }
