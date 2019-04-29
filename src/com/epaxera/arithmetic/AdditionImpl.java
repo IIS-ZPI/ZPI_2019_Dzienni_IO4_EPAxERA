ad28e4ac90f73a1149660e62d828059145061055
Patryk Gitner <209317@edu.p.lodz.pl>
2019-04-29 17:44:13 +0200

    Drugi komentarz

--- src/com/epaxera/arithmetic/AdditionImpl.java
+++ src/com/epaxera/arithmetic/AdditionImpl.java
@@ -1,7 +1,7 @@
 package com.epaxera.arithmetic;
 
 public class AdditionImpl implements Additionable {
-
+    // drugi komentarz
     @Override public double addition(double a, double b) {
         return a + b;
     }
