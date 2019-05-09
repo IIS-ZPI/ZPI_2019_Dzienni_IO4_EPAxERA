a11457e77c6304efb24a0b2465d39d01ca7cc84a
Patryk Gitner <209317@edu.p.lodz.pl>
2019-05-09 17:34:08 +0200

    Drugi komentarz - Patryk

--- src/com/epaxera/arithmetic/Differenciable.java
+++ src/com/epaxera/arithmetic/Differenciable.java
@@ -1,6 +1,7 @@
 package com.epaxera.arithmetic;
 
 public interface Differenciable {
+    // patryk - drugi komentarz
 
     // pierwszy komentarz - Dawid
     double difference(double a, double b);
