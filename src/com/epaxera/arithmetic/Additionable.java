6ad1787f072d6830033f139dcb1409d1ce458c8d
DawiQ <blodawiq@gmail.com>
2019-05-09 17:11:52 +0200

    Komentarz do revertowania

--- src/com/epaxera/arithmetic/Additionable.java
+++ src/com/epaxera/arithmetic/Additionable.java
@@ -1,6 +1,9 @@
 package com.epaxera.arithmetic;
 
 public interface Additionable {
+
+    // dawid komentuje: komentrarrz do zrevertowania
+
     double addition(double a, double b);
 
     //fourth-comment
