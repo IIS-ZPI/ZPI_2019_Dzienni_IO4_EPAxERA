27fac50507b58cd065c30e912bde4d48dfe59b2f
dettlaffb <209295@edu.p.lodz.pl>
2019-05-09 16:30:00 +0200

    seventh comm

--- src/com/epaxera/arithmetic/DifferenceImpl.java
+++ src/com/epaxera/arithmetic/DifferenceImpl.java
@@ -1,6 +1,7 @@
 package com.epaxera.arithmetic;
 
 public class DifferenceImpl implements Differenciable{
+    //siup
 
     @Override public double difference(double a, double b) {
         return 0;
