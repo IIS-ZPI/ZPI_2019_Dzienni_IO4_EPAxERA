c20d312366db889040f7e8067d7916b1e84b3b73
Kamil Jurek <informator017@wp.pl>
2019-05-09 16:31:06 +0200

    Dodaje klase implementującą mnożenie.

--- src/com/epaxera/arithmetic/DifferenceImpl.java
+++ src/com/epaxera/arithmetic/DifferenceImpl.java
@@ -2,6 +2,7 @@ package com.epaxera.arithmetic;
 
 public class DifferenceImpl implements Differenciable{
 
+    //Fifth comment
     @Override public double difference(double a, double b) {
         return 0;
     }
