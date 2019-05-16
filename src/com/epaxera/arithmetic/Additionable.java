12b87217581dff7ae0ba10444c7d4d4c83019a59
Patryk Gitner <209317@edu.p.lodz.pl>
2019-05-16 17:14:23 +0200

    Dodanie struktury projektu Angular oraz Spring Boot

--- src/com/epaxera/arithmetic/Additionable.java
+++
@@ -1,5 +0,0 @@
-package com.epaxera.arithmetic;
-
-public interface Additionable {
-    double addition(double a, double b);
-}
--- src/com/epaxera/arithmetic/Differenciable.java
+++
@@ -1,5 +0,0 @@
-package com.epaxera.arithmetic;
-
-public interface Differenciable {
-    double difference(double a, double b);
-}
