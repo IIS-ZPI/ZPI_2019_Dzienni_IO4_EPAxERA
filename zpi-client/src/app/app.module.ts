7f610ec145b8f1820f735efe329134d62df6922a
Kamil Jurek <informator017@wp.pl>
2019-05-30 17:19:31 +0200

    Dodanie formatki dla miar statystycznych oraz ilości sesji.

--- zpi-client/src/app/app.module.ts
+++ zpi-client/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { HomeComponent } from './components/home/home.component';
+import { CurrencyDecayComponent } from './components/currency-decay/currency-decay.component';
+import { SessionsComponent } from './components/sessions/sessions.component';
+import { StatisticalDataComponent } from './components/statistical-data/statistical-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CurrencyDecayComponent,
+    SessionsComponent,
+    StatisticalDataComponent
   ],
   imports: [
     BrowserModule,
