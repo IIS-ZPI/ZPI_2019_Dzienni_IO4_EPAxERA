9bdddee71fcc32142a8d8c8f808d9baa609c7eb3
209515 <209515@L404-14>
2019-06-06 17:35:59 +0300

    Input values into graph component

--- zpi-client/src/app/components/test-graph/test-graph.component.ts
+++ zpi-client/src/app/components/test-graph/test-graph.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import * as CanvasJS from './canvasjs.min';
 
 @Component({
@@ -7,22 +7,26 @@ import * as CanvasJS from './canvasjs.min';
   styleUrls: ['./test-graph.component.scss']
 })
 export class TestGraphComponent implements OnInit {
+  @Input() title: string;
+  @Input() subtitle: string;
+
   ngOnInit() {
     let dataPoints = [];
     let y = 0;
-    for ( var i = 0; i < 90; i++ ) {
+    for ( var i = 0; i < 98; i++ ) {
       y += Math.round(5 + Math.random() * (-5 - 5));
       dataPoints.push({ y: y});
     }
+
     let chart = new CanvasJS.Chart("chartContainer", {
       zoomEnabled: true,
       animationEnabled: true,
       exportEnabled: true,
       title: {
-        text: "Performance Demo - 10000 DataPoints"
+        text: this.title
       },
       subtitles:[{
-        text: "Try Zooming and Panning"
+        text: this.subtitle
       }],
       data: [
         {
